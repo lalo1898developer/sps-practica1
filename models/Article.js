dc82303e695ea017a604488da667960971eaf810
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-09-24 16:10:26 -0500

    Crea el modelo de Article

---
+++ models/Article.js
@@ -0,0 +1,64 @@
+const Article = [
+    {
+        id: 1,
+        title: "Titulo 1",
+        message: [
+            {paragraph: "parrafo1"},
+            {paragraph: "parrafo2"},
+            {paragraph: "parrafo3"},
+            {paragraph: "parrafo4"}
+        ],
+        date: "01/01/2020",
+        autor: "Autor 1"
+    },
+    {
+        id: 2,
+        title: "Titulo 2",
+        message: [
+            {paragraph: "parrafo1"},
+            {paragraph: "parrafo2"},
+            {paragraph: "parrafo3"},
+            {paragraph: "parrafo4"}
+        ],
+        date: "02/02/2020",
+        autor: "Autor 2"
+    },
+    {
+        id: 3,
+        title: "Titulo 3",
+        message: [
+            {paragraph: "parrafo1"},
+            {paragraph: "parrafo2"},
+            {paragraph: "parrafo3"},
+            {paragraph: "parrafo4"}
+        ],
+        date: "03/03/2020",
+        autor: "Autor 3"
+    },
+    {
+        id: 4,
+        title: "Titulo 4",
+        message: [
+            {paragraph: "parrafo1"},
+            {paragraph: "parrafo2"},
+            {paragraph: "parrafo3"},
+            {paragraph: "parrafo4"}
+        ],
+        date: "04/04/2020",
+        autor: "Autor 4"
+    },
+    {
+        id: 5,
+        title: "Titulo 5",
+        message: [
+            {paragraph: "parrafo1"},
+            {paragraph: "parrafo2"},
+            {paragraph: "parrafo3"},
+            {paragraph: "parrafo4"}
+        ],
+        date: "05/05/2020",
+        autor: "Autor 5"
+    },
+];
+
+module.exports = Article;
