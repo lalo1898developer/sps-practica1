a402e3ddd51169e323451409170f7492de6d912e
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-09-24 18:17:47 -0500

    Añade el middleware de las rutas que fueron creadas

--- api/index.js
+++ api/index.js
@@ -6,10 +6,12 @@ const morgan = require('morgan');
 const api = express();
 const PORT = process.env.PORT || 3000;
 
-// Definimos los siguientes middlewares para utilizar morgan y evitar que nos salgan los warnings en consola de Express
+// Definimos los siguientes middlewares para utilizar morgan y evitar que nos salgan los warnings en consola de Express.
+// Agregamos el middleware que definira la ruta principal de nuestros endpoints
 api.use(morgan('dev'));
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ express: true }));
+api.use('/api/sps/helloworld/v1/', require('../routers'));
 
 // Establecemos el endpoint para probar que el servidor responsa y un mensaje para indentificar esta acción
 api.get('/', (req, res) => res.send('Esta viva y funcionando!'));
