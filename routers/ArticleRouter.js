ca344ec2913d796acb2c4829fa5bb55e8f6df3c6
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-09-24 18:54:48 -0500

    Añade comentaios para la documentación de Swagger

--- routers/ArticleRouter.js
+++ routers/ArticleRouter.js
@@ -5,10 +5,42 @@ const express = require('express');
 const router = express.Router();
 
 // Importamos la constante que contiene las funciones de nuestro controlador
+
 const { ArticleController } = require('../controllers');
 
 // Declaramos las rutas de nuestros endpoint junto con su funcion correspondiente
+
+/**
+ * @swagger
+ * /api/sps/helloworld/v1/read:
+ *  get:
+ *   description: Regresa un Array con todos los articles como objeto
+ *   responses:
+ *    '200':
+ *      description: Petición Exitosa
+ *    '400':
+ *      description: Error en la petición
+ */
 router.get('/read', ArticleController.read);
+
+/**
+ * @swagger
+ * /api/sps/helloworld/v1/create:
+ *  post:
+ *   description: Agrega un nuevo article al modelo
+ *   parameters:
+ *     - in: body
+ *       id: Id
+ *       Title: Titulo
+ *       message: Mensaje que contiene, seperado en parrafos
+ *       date: Fecha de creacion del articulo
+ *       autor: Autor
+ *   responses:
+ *    '201':
+ *      description: Petición Exitosa
+ *    '400':
+ *      description: Error en la petición
+ */
 router.post('/create', ArticleController.create);
 
 // Exportamos nuestra constante
