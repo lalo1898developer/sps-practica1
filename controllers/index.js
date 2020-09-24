c89120257c56cfb89f5397cc9145eade8e11d826
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-09-24 18:27:32 -0500

    Exporta el controlador de article para que sea usado por los endpoints

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+// Importanmos los metodos a una constante de nuestro controlador
+const ArticleController = require('./ArticleController');
+
+// Exportamos esta constante para que sean usados los metodos que contiene por los endpoints
+module.exports = {
+    ArticleController,
+};
